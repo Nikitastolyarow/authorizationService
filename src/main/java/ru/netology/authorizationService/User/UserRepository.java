7c918632ae60c20e0727801d5caaffd0ad035cd8
Nikitastolyarow <nikita.stolyarow2002@yandex.ru>
2024-11-03 12:13:44 +0300

    Update UserRepository

--- src/main/java/ru/netology/authorizationService/User/UserRepository.java
+++ src/main/java/ru/netology/authorizationService/User/UserRepository.java
@@ -8,26 +8,23 @@ import java.util.Map;
 
 @Repository
 public class UserRepository {
-//        if("Nikita".equals(user) && "12345".equals(password)){
-//            return List.of(Authorities.READ , Authorities.WRITE);
-//        }
 
-         Map<String,String> userAndPassword = Map.of(
-                "Nikita", "12345",
-                "Anna", "67890",
-                "John", "54321"
-        );
+    private final Map<String, String> userAndPassword = Map.of(
+            "Nikita", "12345",
+            "Anna", "67890",
+            "John", "54321"
+    );
 
-         Map<String, List<Authorities>> userAuthorities = Map.of(
-                 "Nikita" , List.of(Authorities.READ , Authorities.WRITE),
-                 "Anna" , List.of(Authorities.READ , Authorities.WRITE),
-                 "John" , List.of(Authorities.READ , Authorities.WRITE)
-         );
+    private final Map<String, List<Authorities>> userAuthorities = Map.of(
+            "Nikita", List.of(Authorities.READ, Authorities.WRITE),
+            "Anna", List.of(Authorities.READ, Authorities.WRITE),
+            "John", List.of(Authorities.READ, Authorities.WRITE)
+    );
 
     public List<Authorities> getUserAuthorities(String user, String password) {
-            if (userAndPassword.containsKey(user) && userAndPassword.containsKey(password)) {
-                return userAuthorities.get(user);
-            }
-        return List.of();
+        if (userAndPassword.containsKey(user) && userAndPassword.get(user).equals(password)) {
+            return userAuthorities.get(user);
         }
+        return List.of();
+    }
 }
